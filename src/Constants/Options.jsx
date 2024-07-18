ccc1d9d3d5ba9cc90cdc95bd90f08713b84fbd64
SALEHIN <msionlinekingdom@gmail.com>
2024-07-18 16:54:33 +0600

    cost and travels list added

--- src/Constants/Options.jsx
+++ src/Constants/Options.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A sole travels in exploration',
+        icon: '/public/single-trip.png',
+        people: '1'
+    },
+    {
+        id:2,
+        title:'A Couple',
+        desc:'Two travels in tandem',
+        icon: '/public/couple-trip.png',
+        people: '2 People'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon: '/public/Family-trip.png',
+        people: '3 to 5 People'
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'A bunch of thrilled-seeks',
+        icon: '/public/friend-trip.png',
+        people: '5 to 10 People'
+    },
+]
 
-const Options = props => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-Options.propTypes = {
-    
-};
-
-export default Options;
+export const selectBudgetOptions=[
+    {
+        id:1,
+        title: 'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: '/public/cheap-price.png'
+    },
+    {
+        id:2,
+        title: 'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: '/public/moderate-cost.png'
+    },
+    {
+        id:3,
+        title: 'Luxury',
+        desc: 'Do not worry about cost',
+        icon: '/public/luxury-cost.png'
+    },
+]
